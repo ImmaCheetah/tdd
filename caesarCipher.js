9e1283d3d9caba085ac36663b92b7bfda1974bef
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-04-30 11:27:37 -0400

    Add more tests

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,23 +1,37 @@
 function caesarCipher(string, shiftValue) {
-    return 'BCDEFG'
-}
+    let cipherText = '';
 
-function createCipherTextArray(shiftValue) {
-    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    let cipherArray = [];
-
-    for (let i = 0; i < alphabet.length; i++) {
-        cipherArray[i + shiftValue] = alphabet[i];
+    if (!shiftValue) {
+        return string;
     }
 
-    return cipherArray;
+    for (let i = 0; i < string.length; i++) {
+        let currentCharCode = string.charCodeAt(i) + shiftValue;
+        if (currentCharCode > 90) {
+            currentCharCode = 65;
+        }
+        cipherText += (String.fromCharCode(currentCharCode).toUpperCase());
+    }
+
+    return cipherText;
+
 }
 
-console.log(createCipherTextArray(1));
 
 // create alphabet array
 // use shift value to loop through alphabet array and add value to each index and store that letter 
 // for encryption convert string to array
 // take first letter and search in alphabet, when found use same index from cipher array
 
+// function createCipherTextArray(shiftValue) {
+//     let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+//     let cipherArray = new Array(26).fill(null);
+
+//     for (let i = 0; i < alphabet.length; i++) {
+//         cipherArray[i + shiftValue] = alphabet[i];
+//     }
+//     return cipherArray;
+// }
+
+// console.log(createCipherTextArray(9));
 export {caesarCipher}
--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -1,15 +1,48 @@
 import { caesarCipher } from '../caesarCipher'
 
-test('should exist', () => {
-    expect('./caesarCipher').toBeDefined();
+
+test('caesar function exists/defined', () => {
+    expect(caesarCipher).toBeDefined();
 })
 
-test('caesar function works', () => {
-    expect(caesarCipher).toBeDefined();
+test('works with 1 letter', () => {
+    expect(caesarCipher('A', 1)).toEqual('B');
+})
+
+test('returns string if no shift value', () => {
+    expect(caesarCipher('hello')).toEqual('hello');
 })
 
 test('encrypts lowercase word with no punctuation and shift of 1', () => {
     let expected = 'BCDEFG';
     let actual = caesarCipher('abcdef', 1);
+    expect(actual).toEqual(expected);
+})
+
+test('works with shift value bigger than 1', () => {
+    let expected = 'CDEFGHIJKLMNOPQRSTUVWXYZAB';
+    let actual = caesarCipher('abcdefghijklmnopqrstuvwxyz', 2);
+    expect(actual).toEqual(expected);
+})
+
+test('works with uppercase', () => {
+    let expected = 'BCD';
+    let actual = caesarCipher('ABC', 1);
+    expect(actual).toEqual(expected);
+})
+
+test('works with uppercase AND lowercase', () => {
+    let expected = 'BCD';
+    let actual = caesarCipher('aBC', 1);
+    expect(actual).toEqual(expected);
+})
+
+// test('works with shift value bigger than string length', () => {
+//     let expected = 
+// })
+
+test.skip('encrypts word with punctuation', () => {
+    let expected = 'IFMMP?';
+    let actual = caesarCipher('hello,', 1);
     expect(actual).toEqual(expected);
 })
